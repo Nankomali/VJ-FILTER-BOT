4607438d135103111a05c5ba6adee386c2329df0
Nankomali <98003797+Nankomali@users.noreply.github.com>
2024-01-21 23:23:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,7 +49,7 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://gobinath474708:gobi474709@cluster0.alxfa7h.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Nankomali2")
+DATABASE_NAME = environ.get('DATABASE_NAME', "gobinath474708")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
