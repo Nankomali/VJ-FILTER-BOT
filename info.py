f52bf1abde16fa7063865e5ea24ec82832f2b4ce
Nankomali <98003797+Nankomali@users.noreply.github.com>
2025-05-07 20:49:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -125,7 +125,7 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', 'eab5f3bcc789
 
 
 # Shortlink Info
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False)) # Set True Or False
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'https://tnlinks.in')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'eab5f3bcc789bf4f9fc18a4275dbece7ee758589')
 TUTORIAL = environ.get('TUTORIAL', '') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
